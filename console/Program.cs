b8dfc1a00466e64cb2712665ce39ef2b3d7ff3b3
Christof Sprenger <christof>
2019-12-04 21:44:54 -0800

    move extension to separate file

--- console/Program.cs
+++ console/Program.cs
@@ -6,24 +6,6 @@
         IRandom Next<T>(out T value);
     }
 
-    public static class RandomExtensions
-    {
-
-        public static IRandom Next(this IRandom rand, int lower, int upper, out int value)
-        {
-            var r = rand.Next<int>(out value);
-            value = value % (upper - lower) + lower;
-            return r;
-        }
-
-        public static IRandom Next(this IRandom rand, double lower, double upper, out double value)
-        {
-            var r = rand.Next<double>(out value);
-            value = value * (upper - lower) + lower;
-            return r;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
